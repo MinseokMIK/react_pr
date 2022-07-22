3b03e84b29c369a5b286767f1db6757723480143
Minsuk Kim <msk7316@gmail.com>
2022-07-23 01:09:59 +0900

    에러 해결

--- todo.js
+++ todo.js
@@ -1,2 +1,9 @@
-const wraps = document.getElementsByClassName("wrap")
+const wraps = document.getElementsByClassName("wrap");
+
 console.log(wraps);
+
+const title = document.getElementById("title");
+
+console.log(title);
+
+title.style.backgroundColor = "yellow";
